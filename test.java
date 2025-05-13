48b3bfe1d9e384f194dea2d68c12d135b17cedb1
jungyeongmo7 <ballondor7@naver.com>
2025-05-13 12:02:34 +0900

    ffff

--- test.java
+++ test.java
@@ -5,9 +5,7 @@ public class test{
         System.out.println("hello,my world");
         System.out.println("hello,my name is...");
         System.out.println("ppppppp...");
-        int x,y;
-        Scanner scan=new Scanner(System.in);
-        x=scan.nextInt();
+        System.out.println("fffffff");
     }
 
 }
