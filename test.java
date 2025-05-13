cec9bdc6d88afe37bbfcc93d3ea1b4142127fcc0
jungyeongmo7 <ballondor7@naver.com>
2025-05-13 11:28:44 +0900

    commit

--- test.java
+++ test.java
@@ -1,6 +1,7 @@
 public class test{
     public static void main(String[] args){
-        System.out.print("hello,my world");
+        System.out.println("hello,my world");
+        System.out.println("hello,my name is...");
     }
 
 }
